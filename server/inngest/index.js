a1646488ba1b23132116c3977efa83882fa82f04
Subharthy Kuiry <subharthykuiry@gmail.com>
2025-08-29 10:23:08 +0530

    Fix errors and push project again

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -211,7 +211,8 @@ const deleteStory = inngest.createFunction(
 // send notification of unseen messages
 const sendNotificationOfUnseenMessages = inngest.createFunction(
   {id:'send-unseen-messages-notification'},
-  {cron:"TZ=America/New_York 0 9 * * * "},
+  { cron: "TZ=Asia/Kolkata 0 9 * * *" },
+
   async ({step})=>{
     const messages = await Message.find({seen:false,}).populate('to_user_id')
     const unseenCount = {}
